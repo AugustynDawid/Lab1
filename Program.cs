f00d9b3c9d38abb904f1ce7de34bba6a1ebf10fd
AugustynDawid <d.augustyn@interia.pl>
2022-07-10 12:19:22 +0200

    Przykład użycia klasy Fraction

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fraction x = new Fraction(12, 31);
+            Fraction y = new Fraction(6, -9);
+            Fraction z = new Fraction(2, 18);
+
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine($"y = {y}");
+            Console.WriteLine($"z = {z}");
+
+            Fraction xy = x * y;
+
+            Console.WriteLine($"xy = {xy}");
+
+            Fraction[] tab = new Fraction[] { x, y, z, xy };
+            Array.Sort(tab);
+
+            string sorted = String.Join<Fraction>(" < ", tab);
+            Console.WriteLine($"Sorted: {sorted}");
+
         }
     }
 }
